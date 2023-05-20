013b25f3596219bf916545c1c88ad05be193a764
Ari Birnbaum <ari@ceiphr.com>
2023-05-20 19:31:55 -0400

    Added sandpack support. Added remark-capitalize support. Added highlightjs support. Added katex support.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import '@styles/globals.css';
+import '@styles/highlight/dark.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
