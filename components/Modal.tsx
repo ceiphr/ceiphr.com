632cf99646b5bdb7ceab8a343a07a3b6d1fdbf23
Ari Birnbaum <ari@ceiphr.com>
2023-06-04 21:46:04 -0400

    Refactored prompt.

---
+++ components/Modal.tsx
@@ -0,0 +1,53 @@
+import { Fragment, FunctionComponent, ReactNode, useEffect } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
+
+interface Props {
+    children: ReactNode;
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+}
+
+const Modal: FunctionComponent<Props> = ({ children, isOpen, setIsOpen }) => {
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog
+                as="div"
+                className="relative z-10"
+                onClose={() => setIsOpen(false)}
+            >
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl border border-gray-800 bg-black p-6 text-left align-middle shadow-xl transition-all">
+                                {children}
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    );
+};
+
+export default Modal;
--- components/blog/Prompt.tsx
+++ components/blog/Prompt.tsx
@@ -1,15 +1,15 @@
-import {
-    Fragment,
-    FunctionComponent,
-    useCallback,
-    useEffect,
-    useRef,
-    useState
-} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-import { Dialog, Transition } from '@headlessui/react';
-import { TbSend as SendIcon } from 'react-icons/tb';
+import classNames from 'classnames';
+import { TbSend as SendIcon, TbUserCircle as UserIcon } from 'react-icons/tb';
+import TextareaAutosize from 'react-textarea-autosize';
 import useLLM, { OpenAIMessage } from 'usellm';
+
+import Logo from '@assets/icons/Logo';
+import Modal from '@components/Modal';
+
+// TODO https://github.com/usellm/usellm/blob/main/usellm.org/app/demo/document-qna/page.tsx
+// TODO Add markdown support
 
 const createPrompt = (paragraphs: string[], question: string) => `
 Read the following technical article and answer the question below.
@@ -23,102 +23,33 @@ ${paragraphs.join('\n\n')}
 Question: ${question}
 `;
 
-interface ExpandingTextareaProps {
-    input: string;
-    handleInput: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-}
-
-// TODO Get the textarea to expand and shrink based on the content
-const ExpandingTextarea: FunctionComponent<ExpandingTextareaProps> = ({
-    input,
-    handleInput
-}) => {
-    const DEFAULT_HEIGHT = 32,
-        MAX_HEIGHT = 128;
-    const textareaRef = useRef<HTMLTextAreaElement>(null);
+const Message = ({ role, content }: OpenAIMessage) => {
+    const iconProps = {
+        className: 'text-gray-500 w-5 h-6'
+    };
+    const Icon = () =>
+        role === 'user' ? <UserIcon {...iconProps} /> : <Logo {...iconProps} />;
 
     return (
-        <textarea
-            ref={textareaRef}
-            onChange={handleInput}
-            value={input}
-            placeholder="Ask ChatGPT about this article..."
-            className="w-full h-full overflow-hidden placeholder-gray-700 bg-transparent resize-none focus:outline-none px-3 py-2"
-        />
-    );
-};
-
-interface ModalProps {
-    children: React.ReactNode;
-}
-
-const Modal: FunctionComponent<ModalProps> = ({ children }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    useEffect(() => {
-        function handleKeyDown(e: KeyboardEvent) {
-            // Check if either ctrl or cmd is pressed along with 'k'
-            if ((e.ctrlKey || e.metaKey) && e.key === 'k') {
-                e.preventDefault();
-                setIsOpen((isOpen) => !isOpen);
-            }
-        }
-        window.addEventListener('keydown', handleKeyDown);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, []);
-
-    return (
-        <Transition appear show={isOpen} as={Fragment}>
-            <Dialog
-                as="div"
-                className="relative z-10"
-                onClose={() => setIsOpen(false)}
-            >
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur" />
-                </Transition.Child>
-
-                <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center p-4 text-center">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
-                            <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl border border-gray-800 bg-black p-6 text-left align-middle shadow-xl transition-all">
-                                {children}
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
-                </div>
-            </Dialog>
-        </Transition>
+        <p
+            className={classNames(
+                'flex flex-row',
+                role === 'user' && 'text-gray-500'
+            )}
+        >
+            <span className="mr-2">
+                <Icon />
+            </span>
+            {content}
+        </p>
     );
 };
 
 const Prompt = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const [input, setInput] = useState('');
     const [history, setHistory] = useState<OpenAIMessage[]>([]);
     const llm = useLLM({ serviceUrl: '/api/llm' });
-
-    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInput(e.target.value);
-    };
 
     const handleSend = useCallback(() => {
         if (input.length === 0) return;
@@ -144,42 +75,57 @@ const Prompt = () => {
                 e.preventDefault();
                 handleSend();
             }
+
+            // Check if either ctrl or cmd is pressed along with 'k'
+            if ((e.ctrlKey || e.metaKey) && e.key === 'k') {
+                e.preventDefault();
+                setIsOpen(!isOpen);
+            }
         }
         window.addEventListener('keydown', handleKeyDown);
 
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, [handleSend]);
+    }, [handleSend, isOpen]);
 
     return (
-        <Modal>
-            <Dialog.Title
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-300"
-            >
-                ChatGPT{' '}
-                <code className="ml-1 px-1 py-0.5 bg-gray-900 border-gray-800 border rounded-md">
-                    3.5
-                </code>
-            </Dialog.Title>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+            <h3 className="text-lg font-medium leading-6 text-gray-300">
+                Ask A.R.I. (Artificial Reasoning Intelligence) about this
+                article.
+            </h3>
             <div className="mt-2">
                 {history.map((message, i) => (
-                    <p key={`message-${i}`} className="text-sm text-gray-500">
-                        {message.role === 'user' ? (
-                            <span className="text-gray-300">You: </span>
-                        ) : (
-                            <span className="text-gray-400">ChatGPT: </span>
-                        )}
-                        {message.content}
-                    </p>
+                    <Message key={i} {...message} />
                 ))}
             </div>
-            <div className="relative mt-4 border rounded-lg border-gray-800 flex flex-col flex-grow items-end">
-                <ExpandingTextarea input={input} handleInput={handleInput} />
+            <div className="flex flex-row justify-center pt-2 pb-5 -mb-2 bg-gradient-to-t from-5% from-black">
+                <p className="text-sm text-gray-600">
+                    A.R.I. is powered by{' '}
+                    <a
+                        href="https://openai.com/blog/openai-api/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="underline hover:text-gray-400"
+                    >
+                        ChatGPT 3.5
+                    </a>{' '}
+                    and may produce inaccurate information.
+                </p>
+            </div>
+            <div className="relative border rounded-lg border-gray-800 flex flex-col flex-grow items-end">
+                <TextareaAutosize
+                    className="w-full placeholder-gray-700 bg-transparent resize-none focus:outline-none px-3 py-2"
+                    maxRows={5}
+                    placeholder="Send a message..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
                 <button
                     disabled={input.length === 0}
-                    className="duration-500 absolute bottom-0 p-2 m-1 bg-gray-800 rounded-md disabled:opacity-50 disabled:text-gray-500 disabled:hover:bg-gray-800 text-gray-300 hover:bg-gray-700 focus:outline-none"
+                    onClick={handleSend}
+                    className="duration-500 absolute bottom-0 p-1.5 m-1 bg-gray-800 rounded-md disabled:opacity-50 disabled:text-gray-500 disabled:hover:bg-gray-800 text-gray-300 hover:bg-gray-700 focus:outline-none"
                 >
                     <SendIcon className="w-5 h-5" />
                 </button>
